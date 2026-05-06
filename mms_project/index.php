befb5eb1c42eb28d0cb4197b182c506bc6f46e01
donika <donika.pllana@digitalschool.tech>
2026-05-06 19:47:23 +0200

    mms_project

---
+++ mms_project/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Management System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+<form method="POST">
+
+  <div class="mb-3">
+    <input type="email" 
+      class="form-control" 
+      id="exampleInputEmail1" 
+      placeholder="Name"
+      name="name"
+      required>
+  </div>
+
+  <div class="mb-3">
+    <input type="password" 
+      class="form-control" 
+      id="exampleInputPassword1" 
+      placeholder="Username"
+      name="username"
+      required>
+  </div>
+
+  <div class="mb-3">
+    <input type="password" 
+      class="form-control" 
+      id="exampleInputPassword1" 
+      placeholder="Email"
+      name="email"
+      required>
+  </div>
+
+  <div class="mb-3">
+    <input type="password" 
+    class="form-control" 
+    id="exampleInputPassword1" 
+    placeholder="Password"
+    name="password"
+    required>
+  </div>
+
+  <div class="mb-3">
+    <input type="password" 
+    class="form-control" 
+    id="exampleInputPassword1" 
+    placeholder="Confirm Password"
+    name="confirm_password"
+    required>
+  </div>
+
+  <div class="mb-3 form-check">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+
+</form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
