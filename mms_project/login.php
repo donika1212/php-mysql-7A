140641e91a142670282e6792f9901e8085c6e21c
donika <donika.pllana@digitalschool.tech>
2026-05-06 20:39:10 +0200

    create login.php

---
+++ mms_project/login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Management System - Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
+          rel="stylesheet" 
+          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" 
+          crossorigin="anonymous">
+</head>
+
+<body>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+
+            <h2 class="mb-4 text-center">Login</h2>
+
+            <?php if(isset($error)): ?>
+                <div class="alert alert-danger"><?php echo $error; ?></div>
+            <?php endif; ?>
+
+            <form method="POST">
+
+                <div class="mb-3">
+                    <input type="text" 
+                           class="form-control" 
+                           name="username" 
+                           placeholder="Username" 
+                           required>
+                </div>
+
+                <div class="mb-3">
+                    <input type="password" 
+                           class="form-control" 
+                           name="password" 
+                           placeholder="Password" 
+                           required>
+                </div>
+                <button type="submit" name="signup" class="btn btn-primary w-100">Login</button>
+
+                <p class="text-center mt-3">
+                    Don't you have an account? <a href="index.php">Sign Up here</a>
+                </p>
+
+            </form>
+
+        </div>
+    </div>
+</div>
+    
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" 
+        crossorigin="anonymous"></script>
+</body>
+</html>
