7b81de3d1d098f66dd96d3981e4949e102877c31
donika <donika.pllana@digitalschool.tech>
2026-05-13 20:49:32 +0200

    dashboard.php updated

--- mms_project/admin/dashboard.php
+++ mms_project/admin/dashboard.php
@@ -1,19 +1,18 @@
 <?php
 session_start();
-include("config.php");
+include("../config.php");
 
-if(!isset($_SESSION['user_id'])){
-    header("Location: login.php");
+if(!isset($_SESSION['user_id']) || $_SESSION['is_admin'] != 1){
+    header("Location: ../login.php");
     exit();
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Admin Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
           rel="stylesheet" 
           integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" 
@@ -30,10 +29,6 @@ if(!isset($_SESSION['user_id'])){
     <a href="logout.php" class="btn btn-outline-dark btn-sm">Logout</a>
 </div>
 
-<div class="container mt-5">
-    <h3>Welcome to Movie Management System</h3>
-    <p class="text-muted">You are logged in as a regular user.</p>
-</div>
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" 
